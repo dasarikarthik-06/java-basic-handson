1477622492d7505b8bb04c268defc54a1967520e
Karthik Dasari <karthikdasari.1682@gmail.com>
2026-04-27 15:17:36 +0530

    Implements StringUtils class

--- StringUtils.java
+++ StringUtils.java
@@ -1,17 +1,34 @@
 // StringUtils
 
-// class StringUtils {
-//   static boolean containsChar(String string, char a) {
-//     for(int i = 0;i < string.length;i++) {
-//       System.out.println(string[i]);
-//     }
+class StringUtils {
+  static boolean containsChar(String str, char a) {
+   int index = str.indexOf(a);
+   return  index == -1;
+  }
 
-//     return true;
-//   }
-// }
+  static String reverse(String str) {
+    String reversed = "";
+    int length = str.length();
+    
+    for(int i = length - 1;i>=0;i--) {
+      reversed += str.charAt(i);
+    }
+    return reversed;
+  }
 
-// class Main {
-//   public static void main() {
-//     StringUtils("kka", 'r');
-//   }
-// }
+  static String replaceAll(String str, char charToReplace, char replaceChar) {
+    String newString = "";
+    int length = str.length();
+
+    for(int i = 0; i<length; i++) {
+      char currentCharacter = str.charAt(i);
+      if(currentCharacter == charToReplace) {
+        newString += replaceChar;
+      } else {
+         newString+=currentCharacter;
+      }
+    }
+    return newString;
+    
+  }
+}

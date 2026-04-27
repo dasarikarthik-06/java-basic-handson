b5e0981aac28e90140430fe8c3ca0849ddc83dec
Karthik Dasari <karthikdasari.1682@gmail.com>
2026-04-27 15:00:03 +0530

    Implements money class

--- Money.java
+++ Money.java
@@ -1,1 +1,16 @@
 // Money.java
+
+class Money {
+  final int value;
+  Money(int value) {
+    this.value = value;
+  }
+
+  Money add(Money ob) {
+    return new Money (this.value + ob.value);
+  }
+
+  Money minus(Money ob) {
+    return new Money (this.value - ob.value);
+  }
+}
--- StringUtils.java
+++ StringUtils.java
@@ -1,1 +1,17 @@
 // StringUtils
+
+// class StringUtils {
+//   static boolean containsChar(String string, char a) {
+//     for(int i = 0;i < string.length;i++) {
+//       System.out.println(string[i]);
+//     }
+
+//     return true;
+//   }
+// }
+
+// class Main {
+//   public static void main() {
+//     StringUtils("kka", 'r');
+//   }
+// }
